08e5d54c3a50a32d05e780666fdbe9330290307d
hudson-newey <grath@duck.com>
2023-12-17 22:32:08 +1000

    Added the ability to drag and save cards

--- script/dist/draggable.js
+++ script/dist/draggable.js
@@ -8,11 +8,11 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
 var draggableItems = document.querySelectorAll(".draggable");
 var containers = document.querySelectorAll(".container");
 draggableItems.forEach(function (container) {
-    container.addEventListener("dragstart", function (e) {
+    container.addEventListener("dragstart", function () {
         container.classList.add("dragging");
     });
     container.addEventListener("dragend", function (e) {
-        var _a, _b;
+        var _a, _b, _c, _d, _e, _f;
         var afterElement = getDragAfterElement(containers[0], e.clientX);
         var draggingItem = document.querySelector(".dragging");
         if (!draggingItem)
@@ -24,6 +24,31 @@ draggableItems.forEach(function (container) {
             (_b = container.parentNode) === null || _b === void 0 ? void 0 : _b.insertBefore(draggingItem, afterElement);
         }
         draggingItem.classList.remove("dragging");
+        var currentCardOrder = getCookie(CARD_COOKIE_NAME);
+        if (!currentCardOrder)
+            return;
+        var draggingCardUrl = (_d = (_c = draggingItem.querySelector("a")) === null || _c === void 0 ? void 0 : _c.href) === null || _d === void 0 ? void 0 : _d.replace(/\/+$/, "");
+        if (!draggingCardUrl)
+            return;
+        var cards = currentCardOrder.split(",");
+        var draggingCardIndex = cards.indexOf(draggingCardUrl);
+        if (draggingCardIndex === -1)
+            return;
+        cards.splice(draggingCardIndex, 1);
+        // swap the cards
+        var afterElementUrl = (_f = (_e = afterElement.querySelector("a")) === null || _e === void 0 ? void 0 : _e.href) === null || _f === void 0 ? void 0 : _f.replace(/\/+$/, "");
+        if (!afterElementUrl)
+            return;
+        var afterElementIndex = cards.indexOf(afterElementUrl);
+        if (afterElementIndex === -1) {
+            // add it to the end
+            cards.push(draggingCardUrl);
+        }
+        else {
+            cards.splice(afterElementIndex, 0, draggingCardUrl);
+        }
+        removeCookie(CARD_COOKIE_NAME);
+        setCookie(CARD_COOKIE_NAME, cards.join(","));
     });
 });
 function getDragAfterElement(container, x) {
